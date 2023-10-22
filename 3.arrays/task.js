1922b4fc23c399a38ff0f677fcb5fd8ef78afc31
Ditriy Lozichnyy <dima_lozick13@mail.ru>
2023-10-22 17:30:55 +0700

    task 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,26 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.every(
+		(item, index) => item === arr2[index] && arr1.length == arr2.length
+	)
 }
 
+module.exports = {
+	compareArrays,
+}
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let a = users.filter(number => number.gender == gender)
+
+	if (a.length === 0) {
+		return 0
+	}
+
+	let b =
+		a.reduce((acc, item) => {
+			return acc + item.age
+		}, 0) / a.length
+	return b
+}
+
+module.exports = {
+	getUsersNamesInAgeRange,
 }
